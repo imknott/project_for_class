e1ea49969703e7a4fea3fa2b66d4b75a03ec07c3
Ian M Knott <iknott@mail.sfsu.edu>
2023-03-28 18:50:05 +0700

    anotherupdateIan

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,65 @@
+
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
-import 'league_selection.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:project_for_class/dashboard.dart';
+import 'package:project_for_class/league_selection.dart';
+import 'package:project_for_class/new_dash.dart';
+import 'package:project_for_class/onboarding_page.dart';
+import 'package:project_for_class/register_page.dart';
+import 'package:project_for_class/sign_in.dart';
+import 'package:project_for_class/test_data_retrieval.dart';
+import 'package:project_for_class/user_page.dart';
 
-void main() => runApp(const LeagueTable());
+import 'firebase_options.dart';
 
-class LeagueTable extends StatefulWidget {
-  const LeagueTable({Key? key}) : super(key: key);
+//
 
-  @override
-  State<LeagueTable> createState() => _LeagueTableState();
+//want to create a function that takes in the user info provided from the register function such as setting the
+//Future<void> addUserInfo() async {}
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MyApp());
 }
 
-class _LeagueTableState extends State<LeagueTable> {
-
+class MyApp extends StatelessWidget {
+  var _user = FirebaseAuth.instance.currentUser?.email;
   @override
   Widget build(BuildContext context) {
-    /* ASSUME THIS IS THE FINAL PAGE OF THE ONBOARDING PROCESS
-    WHERE THE USER SELECTS THE LEAGUES THEY WANT TO FOLLOW
-     */
-    return const MaterialApp(
-      //home: Dashboard(),
-      home: SelectLeague(),
+    return MaterialApp(
+      initialRoute:
+      FirebaseAuth.instance.currentUser == null ? '/test' : '/newDash',
+      routes: {
+        '/sign-in': (context) {
+          return LoginScreen();
+        },
+        '/test': (context){
+          return const Home();
+        },
+        '/newDash': (context){
+          return MyDashPage(title: '{$_user}');
+        },
+        //  '/forgot-password': (context) {
+        //this will be created soon
+        // },
+        '/register': (context){
+          return RegisterScreen(
+          );
+        },
+        '/onboard': (context){
+          return const OnboardingScreen();
+        },
+       '/user': (context){
+          return const UserPage();
+       }
+      },
     );
   }
 }
